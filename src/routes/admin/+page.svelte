4d90cf0950758d626b1da79c080e86f018e8f591
ShawnEdgell <milky.irl@gmail.com>
2024-09-27 15:32:25 -0400

    admin page

---
+++ src/routes/admin/+page.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+</script>
+
+<svelte:head>
+	<title>Popland - Admin Login</title>
+	<meta
+		name="description"
+		content="Administrator login page for Popland. Manage content and site settings."
+	/>
+</svelte:head>
+
+<section>
+	<h1>Admin</h1>
+	<p></p>
+</section>
+<input
+	type="text"
+	placeholder="Username"
+	class="text-xl mb-2 input input-bordered w-full max-w-xs p-2"
+/>
+<input
+	type="text"
+	placeholder="Password"
+	class="text-xl mb-2 input input-bordered w-full max-w-xs p-2"
+/>
