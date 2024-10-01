938a8140a51efc2e271b580fcfafb073e9f289bf
ShawnEdgell <milky.irl@gmail.com>
2024-10-01 12:26:29 -0400

    added preload, fixed mobile layout, added drawer

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -1,22 +1,49 @@
-<div class="navbar bg-base-100">
-	<div class="flex-1">
-		<a href="/" class="btn btn-ghost text-xl font-black italic">POPLAND</a>
+<div class="drawer">
+	<input id="my-drawer-3" type="checkbox" class="drawer-toggle" />
+	<div class="drawer-content flex flex-col">
+		<!-- Navbar -->
+		<div class="navbar bg-base-300 w-full">
+			<div class="flex-none sm:hidden">
+				<label for="my-drawer-3" aria-label="open sidebar" class="btn btn-square btn-ghost">
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						fill="none"
+						viewBox="0 0 24 24"
+						class="inline-block h-6 w-6 stroke-current"
+					>
+						<path
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							stroke-width="2"
+							d="M4 6h16M4 12h16M4 18h16"
+						></path>
+					</svg>
+				</label>
+			</div>
+			<div class="mx-2 flex-1">
+				<a
+					href="/"
+					class="btn btn-ghost italic font-bold text-xl"
+					data-sveltekit-preload-data="hover">POPLAND</a
+				>
+			</div>
+			<div class="hidden flex-none sm:block">
+				<ul class="menu menu-horizontal">
+					<!-- Navbar menu content here -->
+					<li><a href="/about" data-sveltekit-preload-data="hover">About Us</a></li>
+					<li><a href="/mods" data-sveltekit-preload-data="hover">Mods</a></li>
+					<li><a href="/community" data-sveltekit-preload-data="hover">Community</a></li>
+				</ul>
+			</div>
+		</div>
 	</div>
-	<div class="flex-none">
-		<ul class="menu menu-horizontal px-1">
-			<li class="hidden sm:block"><a href="/about">About Us</a></li>
-			<li class="hidden sm:block"><a href="/mods">Mods</a></li>
-			<li class="hidden sm:block"><a href="/community">Community</a></li>
-			<li class="block sm:hidden">
-				<details>
-					<summary>Menu</summary>
-					<ul class="bg-base-100 rounded-t-none p-2">
-						<li><a href="/about">About Us</a></li>
-						<li><a href="/mods">Mods</a></li>
-						<li><a href="/community">Community</a></li>
-					</ul>
-				</details>
-			</li>
+	<div class="drawer-side">
+		<label for="my-drawer-3" aria-label="close sidebar" class="drawer-overlay"></label>
+		<ul class="menu menu-lg bg-base-200 min-h-full w-80 p-4">
+			<!-- Sidebar content here -->
+			<li><a href="/about" data-sveltekit-preload-data="hover">About Us</a></li>
+			<li><a href="/mods" data-sveltekit-preload-data="hover">Mods</a></li>
+			<li><a href="/community" data-sveltekit-preload-data="hover">Community</a></li>
 		</ul>
 	</div>
 </div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,11 +5,13 @@
 </script>
 
 <div>
-	<div data-theme="light" class="flex min-h-screen flex-col items-center overscroll-none">
-		<div class="fixed top-0 z-10 w-full">
+	<div data-theme="light" class="flex min-h-screen flex-col items-center">
+		<div class="fixed top-0 w-full z-10">
 			<Navbar />
 		</div>
-		<main class="prose lg:prose-xl mt-[4.5rem] flex w-full flex-1 flex-col px-4 py-16">
+		<main
+			class="prose lg:prose-xl mt-[4.5rem] flex w-full flex-1 flex-col px-4 py-16 overscroll-none"
+		>
 			<slot />
 		</main>
 	</div>
